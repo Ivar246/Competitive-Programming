b3e18861f1ca449cf1128f06afc3544ead748097
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-20 22:52:05 +0545

    valid parentheses

---
+++ Valid Parentheses/valid_parentheses.js
@@ -0,0 +1,27 @@
+/* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+condition of validation:
+    -Open brackets must be closed by the same type of brackets.
+    -Open brackets must be closed in the correct order.
+    -Every close bracket has a corresponding open bracket of the same type.
+
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function (s) {
+  const open = ["(", "{", "["];
+  const close = [")", "}", "]"];
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (open.includes(s[i])) {
+      stack.push(s[i]);
+    } else if (open.indexOf(stack.pop()) !== close.indexOf(s[i])) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
