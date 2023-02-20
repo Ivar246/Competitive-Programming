1deddc80a018eb1c92f477f858a5cc9d52e5fe4c
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-20 23:16:11 +0545

    var replaced by let

--- two sum/two_sum.js
+++ two sum/two_sum.js
@@ -8,7 +8,7 @@ return indices of the two numbers such that they add up to target.
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+let twoSum = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++)
     for (let j = i + 1; j < nums.length; j++)
       if (nums[i] + nums[j] === target) return [i, j];
