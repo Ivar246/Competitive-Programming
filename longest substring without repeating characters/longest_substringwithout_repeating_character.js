5742a5ffecdd7ece94ac6540f67493877a114882
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-23 23:11:03 +0545

    longest string without repeating characters

---
+++ longest substring without repeating characters/longest_substringwithout_repeating_character.js
@@ -0,0 +1,17 @@
+/* Given a string s, find the length of the longest 
+substring  without repeating characters
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let stack = [];
+  let len = 0;
+  for (let ch of s) {
+    if (stack.includes(ch)) stack = stack.slice(stack.indexOf(ch) + 1);
+    stack.push(ch);
+    len = stack.length > len ? stack.length : len;
+  }
+  return len;
+};
