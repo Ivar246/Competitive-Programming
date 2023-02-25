b5d7d9c2fbde0b6ffb4135445a558164eebd51c0
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-25 07:39:39 +0545

    correction of output comment

--- longest substring without repeating characters/longest_substringwithout_repeating_character.js
+++ longest substring without repeating characters/longest_substringwithout_repeating_character.js
@@ -17,5 +17,5 @@ var lengthOfLongestSubstring = function (s) {
 };
 
 console.log(lengthOfLongestSubstring("abcabcbb")); // 3
-console.log(lengthOfLongestSubstring("bbbbbb")); // 2
+console.log(lengthOfLongestSubstring("bbbbbb")); // 1
 console.log(lengthOfLongestSubstring("pwwkew")); //3
