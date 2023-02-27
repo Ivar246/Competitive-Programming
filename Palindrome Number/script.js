ee62637786e7e9a48ae26b0dda410fb8be3af5c0
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-27 07:57:36 +0545

    Palindrome number

---
+++ Palindrome Number/script.js
@@ -0,0 +1,20 @@
+/*
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x > 2 ** 31 - 1) return;
+  let rev = 0;
+  let temp = x;
+  while (temp > 0) {
+    rev = rev * 10 + (temp % 10);
+    temp = Math.floor(temp / 10);
+  }
+  return rev === x;
+};
