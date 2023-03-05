9bd4921c9c5fc66bcd7c6c23f84303d12c2727c7
IVAR AHTSERHS <ravistha869@gmail.com>
2023-03-05 15:04:58 +0545

    tiny changes

--- Harmless Ransom note/script.js
+++ Harmless Ransom note/script.js
@@ -12,14 +12,17 @@ function harmlessRansomNote(noteText, magazineText) {
   noteArr.forEach((word) => {
     if (magazineObj[word]) {
       magazineObj[word]--;
+      if (magazineObj < 0) noteIsPossible = false;
     } else noteIsPossible = false;
   });
   return noteIsPossible;
 }
-console.log(
-  harmlessRansomNote(
-    "this is a secret note for you from a secret admirer",
-    "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited"
-  )
-);
+// console.log(
+//   harmlessRansomNote(
+//     "this is a secret note for you from a secret admirer",
+//     "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited"
+//   )
+// );
 
+let arr = { names: 0, hello: 3 };
+if (arr["brad"]) console.log("yes");
